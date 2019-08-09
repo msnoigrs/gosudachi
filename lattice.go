b43ef2d72e1e8d338741db290708e70dac39ebad
Masanao Igarashi <syoux2@gmail.com>
2019-08-09 15:05:49 +0900

    Make sure the best path is properly reversed

--- lattice.go
+++ lattice.go
@@ -260,7 +260,7 @@ func (l *Lattice) GetBestPath() ([]*LatticeNode, error) {
 		ret = append(ret, node)
 	}
 
-	if len(ret) > 2 {
+	if len(ret) > 1 {
 		// reverse
 		for i := len(ret)/2 - 1; i >= 0; i-- {
 			opp := len(ret) - 1 - i
